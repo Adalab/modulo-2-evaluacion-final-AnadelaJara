b2d4f9028c46b7635ca226d9bc78a30a5f338cf8
AnadelaJara <anadelajara@gmail.com>
2021-12-28 21:12:58 +0100

    done part 1 and 2 js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,47 @@
 'use strict';
 
-fetch('https://api.jikan.moe/v3/search/anime?q=naruto')
-.then (response => response.json())
-.then (data => {
-  console.log(data);
-});
+// Pendiente;
+// Crear mensaje por si no encuentra la serie y pintarlo
+// Añadir imagen a las búsquedas sin imagenes
+
+const resultSeries = document.querySelector ('.js_resultSeries');
+const searchInput = document.querySelector ('.js_searchInput');
+const searchButton = document.querySelector ('.js_searchButton');
+//const resetButton = document.querySelector ('.js_resetButton');
+
+let allSeries = [];
+//let favorites = [];
 
 
+function getApiData (){
+  let inputValue = searchInput.value;
 
-//const searchInput = document.querySelector ('.js_searchInput');
+  fetch(`https://api.jikan.moe/v3/search/anime?q=naruto${inputValue}`)
+    .then (response => response.json())
+    .then (data => {
+      allSeries = data.results;
+      getPrintAllSeries ();
+    });
+}
+
+function printHtmlSeries (seriesArticle) {
+  //console.log({seriesArticle});
+  resultSeries.innerHTML += `
+    <img src="${seriesArticle.image_url}" alt="Serie:${seriesArticle.title}"/>
+    <p>${seriesArticle.title}</p>`;
+}
+
+function getPrintAllSeries (){
+  resultSeries.innerHTML='';
+  for (const eachSerieData of allSeries) {
+    printHtmlSeries (eachSerieData);
+  }
+}
+
+
+function handleClickSeries(event) {
+  event.preventDefault();
+  getApiData ();
+}
+searchButton.addEventListener('click', handleClickSeries);
+
