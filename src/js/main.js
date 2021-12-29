24550563e06e21fea76e7617ba6f7355b371fe6f
AnadelaJara <anadelajara@gmail.com>
2021-12-29 20:40:47 +0100

    add js fav functions and  localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,20 @@
 'use strict';
 
-
+/// VARIABLES GLOBALES ///
 const resultSeries = document.querySelector ('.js_resultSeries');
 const favSeries = document.querySelector ('.js_favSeries');
 const searchInput = document.querySelector ('.js_searchInput');
 const searchButton = document.querySelector ('.js_searchButton');
 //const resetButton = document.querySelector ('.js_resetButton');
 
+/// ARRAY ///
+
 let allSeries = [];
 let favorites = [];
 
+/// FUNCIONES ///
+
+// Fetch y ejecuta la función para print en el HTML
 
 function getApiData (){
   let inputValue = searchInput.value;
@@ -22,12 +27,17 @@ function getApiData (){
     });
 }
 
+// Función que imprime en el HTML
+
 function printHtmlSeries (seriesArticle) {
   console.log({seriesArticle});
   resultSeries.innerHTML += `<li class="js_add_favorites" data-id="${seriesArticle.mal_id}">
     <img class="img" src="${seriesArticle.image_url}" alt="Serie:${seriesArticle.title}"/>
     <p>${seriesArticle.title}</p></li>`;
 }
+
+// Función con bucle para recorrer todo el array 
+// Constante / bucle y listener para el click a favoritos (al ser la función donde se recorren todas las series)
 
 function getPrintAllSeries (){
   resultSeries.innerHTML='';
@@ -40,18 +50,33 @@ function getPrintAllSeries (){
   }
 }
 
+//Función para es
+
 function handleAddToFavList (event){
   const selectedSerieId = parseInt(event.currentTarget.dataset.id);
-  console.log(selectedSerieId);
 
-  console.log(`Añadiendo a la lista + "${selectedSerieId}"`);
+  // console.log(`Añadiendo a la lista + "${selectedSerieId}"`);
+  // console.log(allSeries);
+  // console.log(favorites);
 
-
+  //Constante para buscar el id en las series, y que la guarde en una const
   const selectedSerieData = allSeries.find(row => row.mal_id === selectedSerieId);
-  console.log(selectedSerieData);
 
-  favorites.push (selectedSerieData);
+  ///Constante para encontrar las series en el array y las guarde en otra constante 
 
+  const favSeriesData = favorites.find(row => row.mal_id === selectedSerieId);
+
+  //console.log(favSeriesData);
+
+
+  //Condicional para que, si se ha clicado en la serie, no vuelva a añadirse
+  if (favSeriesData === undefined) {
+    favorites.push (selectedSerieData);
+  } else {
+    alert ('Ya añadida a favoritos');
+  }
+  //Llamamos a las funciones:1La que imprime en la sección fav, las favoritas y para guardar en localStorage al estar donde se modifican
+  setFavInLocalStorage ();
   renderFav ();
 }
 
@@ -73,5 +98,31 @@ function handleClickSeries(event) {
   event.preventDefault();
   getApiData ();
 }
+
+//Función para guardar en el localStorage (se llama desde handleAddToFavList)
+
+function setFavInLocalStorage (){
+  console.log('Guardaré en lS');
+  localStorage.setItem ('fav-series', JSON.stringify(favorites) );
+}
+
+function getFavFromLocalStorage () {
+  console.log('Sacaré la lista');
+
+const savedFavContent = localStorage.getItem('fav-series');
+
+if (savedFavContent === null){
+  favorites = [];
+} else {
+  console.log();
+  favorites = JSON.parse(savedFavContent);
+  }
+  renderFav ();
+}
+//Llamamos a la función para coger datos del localS
+getFavFromLocalStorage ();
+
+//Listener para el botón de buscar
+
 searchButton.addEventListener('click', handleClickSeries);
 
