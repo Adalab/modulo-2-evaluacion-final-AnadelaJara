a266413131f3e18a849f1f794a9bca0718d4a6ab
AnadelaJara <anadelajara@gmail.com>
2021-12-29 18:01:59 +0100

    add js fetch and search funct

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,20 @@
 'use strict';
 
-// Pendiente;
-// Crear mensaje por si no encuentra la serie y pintarlo
-// Añadir imagen a las búsquedas sin imagenes
 
 const resultSeries = document.querySelector ('.js_resultSeries');
+const favSeries = document.querySelector ('.js_favSeries');
 const searchInput = document.querySelector ('.js_searchInput');
 const searchButton = document.querySelector ('.js_searchButton');
 //const resetButton = document.querySelector ('.js_resetButton');
 
 let allSeries = [];
-//let favorites = [];
+let favorites = [];
 
 
 function getApiData (){
   let inputValue = searchInput.value;
 
-  fetch(`https://api.jikan.moe/v3/search/anime?q=naruto${inputValue}`)
+  fetch(`https://api.jikan.moe/v3/search/anime?q=${inputValue}&limit=5`)
     .then (response => response.json())
     .then (data => {
       allSeries = data.results;
@@ -25,10 +23,10 @@ function getApiData (){
 }
 
 function printHtmlSeries (seriesArticle) {
-  //console.log({seriesArticle});
-  resultSeries.innerHTML += `
-    <img src="${seriesArticle.image_url}" alt="Serie:${seriesArticle.title}"/>
-    <p>${seriesArticle.title}</p>`;
+  console.log({seriesArticle});
+  resultSeries.innerHTML += `<li class="js_add_favorites" data-id="${seriesArticle.mal_id}">
+    <img class="img" src="${seriesArticle.image_url}" alt="Serie:${seriesArticle.title}"/>
+    <p>${seriesArticle.title}</p></li>`;
 }
 
 function getPrintAllSeries (){
@@ -36,8 +34,40 @@ function getPrintAllSeries (){
   for (const eachSerieData of allSeries) {
     printHtmlSeries (eachSerieData);
   }
+  const allImg = document.querySelectorAll ('.js_add_favorites');
+  for (const addToFavList of allImg) {
+    addToFavList.addEventListener ('click', handleAddToFavList);
+  }
 }
 
+function handleAddToFavList (event){
+  const selectedSerieId = parseInt(event.currentTarget.dataset.id);
+  console.log(selectedSerieId);
+
+  console.log(`Añadiendo a la lista + "${selectedSerieId}"`);
+
+
+  const selectedSerieData = allSeries.find(row => row.mal_id === selectedSerieId);
+  console.log(selectedSerieData);
+
+  favorites.push (selectedSerieData);
+
+  renderFav ();
+}
+
+function renderFav (){
+  favSeries.innerHTML = '';
+
+  for (const eachFavItem of favorites) {
+    renderFavItem (eachFavItem);
+  }
+}
+
+function renderFavItem (eachFavItem) {
+  favSeries.innerHTML += `<li>
+  <img class="img" src="${eachFavItem.image_url}" alt="Serie:${eachFavItem.title}"/>
+  <p>${eachFavItem.title}</p></li>`;
+}
 
 function handleClickSeries(event) {
   event.preventDefault();
